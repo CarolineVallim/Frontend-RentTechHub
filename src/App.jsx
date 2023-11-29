be2523d7ba881d5dd2e3ceaa433c54f8239888e3
Guilherme Silva <guilhermemas2004@gmail.com>
2023-11-29 10:58:25 +0000

    creation of pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import SignUpPage from './Pages/SignUpPage'
-import LoginPage from './Pages/Login'
+import SignUpPage from './Pages/SignUpPage/index.jsx'
+import LoginPage from './Pages/Login/index.jsx'
+import Profile from './Pages/Profile/index.jsx'
+import HomePage from './Pages/HomePage/index.jsx'
+import AllProducts from './Pages/AllProductsPage/index.jsx'
+import Cart from './Pages/Cart/index.jsx'
+import Checkout from './Pages/Checkout/index.jsx'
 
 function App() {
 
   return (
     <div>
       <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/products" element={<AllProducts />}/>
+        <Route path="/cart" element={<Cart />}/>
+        <Route path="/checkout" element={<Checkout />}/>
         <Route path='/signup' element={<SignUpPage/>} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
 
     </div>
---
+++ src/Pages/AllProductsPage/index.jsx
@@ -0,0 +1,7 @@
+export default function AllProducts(){
+    return(
+        <div>
+            <h1>All Products</h1>
+        </div>
+    )
+}
