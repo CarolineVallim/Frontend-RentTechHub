19358b64847cbd4b6fe44d10bde3db8a9b1044a7
Guilherme Silva <guilhermemas2004@gmail.com>
2023-12-01 12:42:18 +0000

    App products page starter code

--- src/Pages/AllProductsPage/index.jsx
+++ src/Pages/AllProductsPage/index.jsx
@@ -1,8 +1,31 @@
-import { useEffect, useState } from "react";
+// import "./index.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 export default function AllProducts(){
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:5005/api/products');
+                console.log("Response from backend:", response.data);
+                setProducts(response.data);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching product data:", error);
+                setError(error.response?.data?.message || error.message);
+                setLoading(false);
+            }
+        };
     
+        fetchData();
+    }, []);
+    
+
 
     return(
         <div>
@@ -10,8 +33,26 @@ export default function AllProducts(){
                 <h1>All Products</h1>
             </div>
             <div>
-            </div>
+                {products.map((product) => (
+                
+                <div className="match-container" key={product._id}>
+                <div>
+                    <img className="competition-flag" src={product.image} />
+                </div>
 
+                <div className="competition-section">
+                    <p>{product.name}</p>
+                    <p>{product.description}</p>
+                </div>
+
+                <div className="buy-section">
+                    <Link to={""}>
+                    <p>Rent</p>
+                    </Link>
+                </div>
+                </div>
+                ))}
+            </div>
         </div>
     )
 }
