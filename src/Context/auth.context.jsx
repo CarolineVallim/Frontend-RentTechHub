8dfe9e541bf045a0c9ec2828b14eb8cc3e129adb
Caroline Vallim <carolinecvallim@gmail.com>
2023-12-07 10:15:39 +0000

    fix cors

--- src/Context/auth.context.jsx
+++ src/Context/auth.context.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "https://rent-tech-hub.adaptable.app";
+const API_URL = "http://localhost:5005";
 
 const AuthContext = React.createContext();
 
